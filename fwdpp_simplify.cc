a894385e2c4b5cb1ebddeffeb3f56a8dccc0c895
molpopgen <krthornt@uci.edu>
2018-01-07 14:32:22 -0800

    new files added

---
+++ fwdpp_simplify.cc
@@ -0,0 +1,7 @@
+// Experimental integration of fwdpp engine
+// with simplification algorithm from Ralph et al.
+// This code starts by copy/pasting several data types
+// and functions from the fwdpy11-based example for that paper.
+
+#include "node.hpp"
+#include "edge.hpp"
