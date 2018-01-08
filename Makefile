9c6511a370c71fc17ea27ccb970060d8cd5fd9c9
molpopgen <krthornt@uci.edu>
2018-01-08 13:42:13 -0800

    compiles, in loose sense of term

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ DEBUG=-DNDEBUG
 DEBUG=
 CXXFLAGS=-std=c++11 -O2 -Wall -W $(DEBUG)
 
-all: fwdpp_lite.o fwdpp_lite_intrusive.o
+all: fwdpp_lite.o fwdpp_lite_intrusive.o fwdpp_simplify.o
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o fwdpp_lite fwdpp_lite.o  -lgsl -lgslcblas -ltcmalloc $(LDFLAGS)
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o fwdpp_lite_intrusive fwdpp_lite_intrusive.o  -lgsl -lgslcblas -ltcmalloc $(LDFLAGS)
 
--- fwdpp_simplify.cc
+++ fwdpp_simplify.cc
@@ -5,22 +5,29 @@
 
 #include <cmath>
 #include <stdexcept>
-
+#include <gsl/gsl_randist.h>
+#include <fwdpp/forward_types.hpp>
+#include <fwdpp/fitness_models.hpp>
+#include <fwdpp/poisson_xover.hpp>
 #include <fwdpp/internal/sample_diploid_helpers.hpp>
 #include <fwdpp/mutate_recombine.hpp>
 #include <fwdpp/sugar/popgenmut.hpp>
+#include <fwdpp/sugar/infsites.hpp>
 #include <fwdpp/sugar/singlepop.hpp>
-#include <fwdpp/sugar/GSLrng_t.h>
-
+#include <fwdpp/sugar/GSLrng_t.hpp>
+#include <fwdpp/fwd_functional.hpp>
+#include <fwdpp/internal/gsl_discrete.hpp>
+#include <fwdpp/internal/gamete_cleaner.hpp>
+#include <fwdpp/util.hpp>
 #include "node.hpp"
 #include "edge.hpp"
 
 using singlepop_t = fwdpp::singlepop<fwdpp::popgenmut>;
-using GSLrng_t = fwdpp::GSLrng_t<GSL_RNG_MT19937>;
+using GSLrng_t = fwdpp::GSLrng_t<fwdpp::GSL_RNG_MT19937>;
 
-template <fitness_function>
+template <typename fitness_function>
 inline fwdpp::fwdpp_internal::gsl_ran_discrete_t_ptr
-w(singlepop_t& pop, const single_locus_fitness_fxn& ff)
+w(singlepop_t& pop, const fitness_function& ff)
 {
     auto N_curr = pop.diploids.size();
     std::vector<double> fitnesses(N_curr);
@@ -30,7 +37,7 @@ w(singlepop_t& pop, const single_locus_fitness_fxn& ff)
             pop.gametes[pop.diploids[i].first].n = 0;
             pop.gametes[pop.diploids[i].second].n = 0;
         }
-    lookup = fwdpp::fwdpp_internal::gsl_ran_discrete_t_ptr(
+    auto lookup = fwdpp::fwdpp_internal::gsl_ran_discrete_t_ptr(
         gsl_ran_discrete_preproc(N_curr, &fitnesses[0]));
     return lookup;
 }
@@ -48,15 +55,14 @@ evolve_generation(const GSLrng_t& rng, singlepop_t& pop,
     auto mutation_recycling_bin
         = fwdpp::fwdpp_internal::make_mut_queue(pop.mcounts);
 
-    auto lookup = w(pop, ...);
+    auto lookup = w(pop, fwdpp::additive_diploid());
     decltype(pop.diploids) offspring(N_next);
 
     // Generate the offspring
-    std::size_t label = 0;
     for (auto& dip : offspring)
         {
-            auto p1 = gsl_ran_discrete_lookup(rng.get(), lookup.get());
-            auto p2 = gsl_ran_discrete_lookup(rng.get(), lookup.get());
+            auto p1 = gsl_ran_discrete(rng.get(), lookup.get());
+            auto p2 = gsl_ran_discrete(rng.get(), lookup.get());
             auto p1g1 = pop.diploids[p1].first;
             auto p1g2 = pop.diploids[p1].second;
             auto p2g1 = pop.diploids[p2].first;
@@ -70,31 +76,29 @@ evolve_generation(const GSLrng_t& rng, singlepop_t& pop,
             if (swap2)
                 std::swap(p2g1, p2g2);
 
-            auto breakpoints = recmodel(pop.gametes[p1g1], pop.gametes[p1g2],
-                                        pop.mutations);
-            dip.first = ancestry_recombination_details(
-                pop, ancestry, gamete_recycling_bin, p1g1, p1g2, breakpoints,
-                pid, std::get<0>(offspring_indexes));
-            breakpoints = recmodel(pop.gametes[p2g1], pop.gametes[p2g2],
-                                   pop.mutations);
-            pid = ancestry.get_parent_ids(p2, swap2);
+            auto breakpoints = recmodel();
+            // dip.first = ancestry_recombination_details(
+            //    pop, ancestry, gamete_recycling_bin, p1g1, p1g2, breakpoints,
+            //    pid, std::get<0>(offspring_indexes));
+            breakpoints = recmodel();
+            // pid = ancestry.get_parent_ids(p2, swap2);
 
-            dip.second = ancestry_recombination_details(
-                pop, ancestry, gamete_recycling_bin, p2g1, p2g2, breakpoints,
-                pid, std::get<1>(offspring_indexes));
+            // dip.second = ancestry_recombination_details(
+            //    pop, ancestry, gamete_recycling_bin, p2g1, p2g2, breakpoints,
+            //    pid, std::get<1>(offspring_indexes));
 
             pop.gametes[dip.first].n++;
             pop.gametes[dip.second].n++;
 
             // now, add new mutations
-            dip.first = fwdpp::mutate_gamete_recycle(
-                mutation_recycling_bin, gamete_recycling_bin, rng.get(), mu,
-                pop.gametes, pop.mutations, dip.first, mmodel,
-                fwdpp::emplace_back());
-            dip.second = fwdpp::mutate_gamete_recycle(
-                mutation_recycling_bin, gamete_recycling_bin, rng.get(), mu,
-                pop.gametes, pop.mutations, dip.second, mmodel,
-                fwdpp::emplace_back());
+            // dip.first = fwdpp::mutate_gamete_recycle(
+            //    mutation_recycling_bin, gamete_recycling_bin, rng.get(), mu,
+            //    pop.gametes, pop.mutations, dip.first, mmodel,
+            //    fwdpp::emplace_back());
+            // dip.second = fwdpp::mutate_gamete_recycle(
+            //    mutation_recycling_bin, gamete_recycling_bin, rng.get(), mu,
+            //    pop.gametes, pop.mutations, dip.second, mmodel,
+            //    fwdpp::emplace_back());
         }
     fwdpp::fwdpp_internal::process_gametes(pop.gametes, pop.mutations,
                                            pop.mcounts);
@@ -107,16 +111,9 @@ evolve_generation(const GSLrng_t& rng, singlepop_t& pop,
 double
 evolve_singlepop_regions_track_ancestry(
     const GSLrng_t& rng, singlepop_t& pop,
-    const std::vector<std::uint32_t>& popsizes, const double mu_selected,
-    const double recrate, const fwdpp::extensions::discrete_mut_model& mmodel,
-    const fwdpp::extensions::discrete_rec_model& rmodel,
-    const double selfing_rate)
+    const std::vector<std::uint32_t>& popsizes, const double mu_neutral,
+    const double mu_selected, const double recrate)
 {
-    if (pop.generation > 0)
-        {
-            throw std::runtime_error(
-                "this population has already been evolved.");
-        }
     const auto generations = popsizes.size();
     if (!generations)
         throw std::runtime_error("empty list of population sizes");
@@ -135,13 +132,25 @@ evolve_singlepop_regions_track_ancestry(
                   * (4. * double(pop.N) * (mu_selected)
                      + 0.667 * (4. * double(pop.N) * (mu_selected)))));
 
-    for (unsigned generation = 0; generation < generations;
-         ++generation, ++pop.generation)
+    fwdpp::poisson_xover recmap(rng.get(), recrate, 0., 1.);
+    unsigned generation = 0;
+    fwdpp::infsites inf;
+    const auto mmodel
+        = [&pop, &rng, &inf, &generation, mu_neutral, mu_selected](
+            fwdpp::traits::recycling_bin_t<singlepop_t::mcont_t>& recbin,
+            singlepop_t::mcont_t& mutations) {
+              return inf(recbin, mutations, rng.get(), pop.mut_lookup,
+                         generation, mu_neutral, mu_selected,
+                         [&rng]() { return gsl_rng_uniform(rng.get()); },
+                         []() { return 0.; }, []() { return 0.; });
+          };
+
+    for (unsigned generation = 0; generation < generations; ++generation)
         {
             const auto N_next = popsizes.at(generation);
-            evolve_generation(rng, pop, N_next, mu_selected, mmodels, recmap);
-            update_mutations(pop.mutations, pop.fixations, pop.fixation_times,
-                             pop.mut_lookup, pop.mcounts, pop.generation,
-                             2 * pop.N);
+            evolve_generation(rng, pop, N_next, mu_selected, mmodel, recmap);
+            fwdpp::update_mutations(pop.mutations, pop.fixations,
+                                    pop.fixation_times, pop.mut_lookup,
+                                    pop.mcounts, generation, 2 * pop.N);
         }
 }
