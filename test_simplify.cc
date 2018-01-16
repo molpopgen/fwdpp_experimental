fd70d4a5674610d14677d8e8ae5e21cb92fb293c
molpopgen <krthornt@uci.edu>
2018-01-15 17:39:44 -0800

    Fix initialization of No.  Use log-time lookup of parents during simplification.  Update the sample list to reflect what the Python script is doing.

--- test_simplify.cc
+++ test_simplify.cc
@@ -1,3 +1,13 @@
+// This file is helping me get an implementation
+// of the simplify algorithm from Ralph et al.
+// implemented in C++.  This implemenation
+// is based on the set theoretic algorithm
+// presented by Jerome Kelleher in the repo
+// of that paper.
+//
+// Almost no intellectual input is from me
+// (KRT). All I did was put the Python
+// implementation in terms of c++11 code.
 #include <cassert>
 #include <tuple>
 #include <vector>
@@ -41,7 +51,7 @@ simplify(const std::vector<std::int32_t>& samples,
                          < std::tie(node_table[b.parent].generation, b.parent,
                                     b.child, b.left);
               });
-    //std::cerr << "sorted: "
+    // std::cerr << "sorted: "
     //          << std::is_sorted(
     //                 edge_table.begin(), edge_table.end(),
     //                 [&node_table](const edge& a, const edge& b) {
@@ -65,7 +75,7 @@ simplify(const std::vector<std::int32_t>& samples,
 
     for (auto& s : samples)
         {
-            No.push_back(make_node(s, node_table[s].generation, 0));
+            No.push_back(make_node(No.size(), node_table[s].generation, 0));
             Ancestry[s].push_back(
                 segment(0, 1, static_cast<std::int32_t>(No.size() - 1)));
         }
@@ -80,9 +90,19 @@ simplify(const std::vector<std::int32_t>& samples,
     for (std::int32_t u = 0; u < static_cast<std::int32_t>(node_table.size());
          ++u)
         {
-            auto f
-                = std::find_if(edge_table.begin(), edge_table.end(),
-                               [u](const edge& e) { return e.parent == u; });
+            // auto f
+            //    = std::find_if(edge_table.begin(), edge_table.end(),
+            //                   [u](const edge& e) { return e.parent == u; });
+
+            // Edges are sorted according to (parent time, parent),
+            // meaning was can find the first element containing
+            // parent u in log time.
+            auto f = std::lower_bound(
+                edge_table.begin(), edge_table.end(), u,
+                [&node_table](const edge& e, const std::int32_t value) {
+                    return std::tie(e.parent, node_table[e.parent].generation)
+                           < std::tie(value, node_table[value].generation);
+                });
             auto l = std::find_if(f, edge_table.end(), [u](const edge& e) {
                 return e.parent != u;
             });
@@ -243,7 +263,7 @@ main(int argc, char** argv)
             in >> a >> b >> x >> y >> std::ws;
             edges.push_back(make_edge(x, y, a, b));
         }
-    simplify({ 0, 1, 2 }, edges, nodes);
+    simplify({ 0, 1, 2, 19, 33, 11, 12 }, edges, nodes);
     std::cout << "nodes:\n";
     for (auto& n : nodes)
         {
