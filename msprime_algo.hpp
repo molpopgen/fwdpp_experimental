76d2231e6ef1f4d0cbf60098f79693043d1a0d79
molpopgen <krthornt@uci.edu>
2018-04-30 18:27:52 -0700

    fix (I think...) implementation of Alg T

--- msprime_algo.hpp
+++ msprime_algo.hpp
@@ -20,6 +20,7 @@
 #include <vector>
 #include <cstdint>
 #include <tuple>
+#include <limits>
 #include "table_collection.hpp"
 
 namespace fwdpp
@@ -84,7 +85,9 @@ namespace fwdpp
         algorithmT(const table_collection& tables)
         // Assumes tables are not empty.  Probably unsafe.
         {
-            std::vector<std::size_t> pi(tables.edge_table.back().parent, 0);
+            std::vector<std::size_t> pi(
+                tables.edge_table.back().parent,
+                std::numeric_limits<std::size_t>::max());
             auto p = fill_I_O(tables);
 
             // Move data for variable name convenience
@@ -96,23 +99,56 @@ namespace fwdpp
             // TODO: replace .at with []
             while (j < M)
                 {
-                    auto p = tables.edge_table[I[j]].parent;
-                    while (j < M && tables.edge_table[I[j]].parent == p
+                    while (j < M
                            && tables.edge_table[I[j]].left == x) // Step T2
                         {
                             auto h = I[j];
                             pi.at(tables.edge_table.at(h).child) = h;
                             ++j;
                         }
+                    // At this point, pi refers to the marginal tree
+                    // beginning at x for all pi[i] != 
+                    // std::numeric_limits<std::size_t>::max()
+                    // The useful thing to do here would 
+                    // be to define a "visitor function",
+                    // taking pi and x as arguments.
+                    //if (x != 0.0)
+                    //    {
+                    //        for (auto& e : tables.edge_table)
+                    //            {
+                    //                std::cerr << e.parent << ' ' << e.child
+                    //                          << ' ' << e.left << ' '
+                    //                          << e.right << '\n';
+                    //            }
+                    //        for (auto& pp : pi)
+                    //            {
+                    //                if (pp
+                    //                    != std::numeric_limits<
+                    //                           std::size_t>::max())
+                    //                    {
+                    //                        std::cout
+                    //                            << tables.edge_table.at(pp)
+                    //                                   .parent
+                    //                            << ' '
+                    //                            << tables.edge_table.at(pp)
+                    //                                   .child
+                    //                            << ' '
+                    //                            << tables.edge_table.at(pp)
+                    //                                   .left
+                    //                            << '\n';
+                    //                    }
+                    //            }
+                    //        std::exit(0);
+                    //    }
                     if (j >= M)
                         break;
                     x = tables.edge_table.at(I.at(j)).left; // T3
-                    p = tables.edge_table.at(O[k]).parent;
-                    while (k < M && tables.edge_table.at(O[k]).parent == p
+                    while (k < M
                            && tables.edge_table.at(O[k]).right == x) // T4
                         {
                             auto h = O.at(k);
-                            pi.at(tables.edge_table.at(h).child) = 0;
+                            pi.at(tables.edge_table.at(h).child)
+                                = std::numeric_limits<std::size_t>::max();
                             ++k;
                         }
                 }
