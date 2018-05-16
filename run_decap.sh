4329708a1e1048f1de137a33e851e95e66e77744
molpopgen <krthornt@uci.edu>
2018-05-16 15:20:35 -0500

    add script to run decap test many times

---
+++ run_decap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in $(seq 1 1 1000)
+do
+    python3 msprime_decapitate.py 10000 500 > /dev/null
+    ./process_decap > /dev/null
+    diff -q msprime_counts.txt cpp_counts.txt
+done    
