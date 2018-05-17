582e815b137b32ded175a808c60c8400c67e998b
molpopgen <krthornt@uci.edu>
2018-05-16 22:29:18 -0500

    more nodes for decapitation testing

--- run_decap.sh
+++ run_decap.sh
@@ -2,7 +2,7 @@
 
 for i in $(seq 1 1 1000)
 do
-    python3 msprime_decapitate.py 10000 500 > /dev/null
+    python3 msprime_decapitate.py 10000 20000 > /dev/null
     ./process_decap > /dev/null
     diff -q msprime_counts.txt cpp_counts.txt
 done    
