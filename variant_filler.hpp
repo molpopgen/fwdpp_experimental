659ffbe0c496ae23b9958ffa8b63c8514dfa424f
molpopgen <krthornt@uci.edu>
2018-08-21 15:17:40 -0700

    Fix variant_filler to correctly iterate over SIBLINGS.  Added various assertions.

--- variant_filler.hpp
+++ variant_filler.hpp
@@ -1,6 +1,7 @@
 #ifndef FWDPP_TS_VARIANT_FILLER_HPP
 #define FWDPP_TS_VARIANT_FILLER_HPP
 
+#include <cassert>
 #include <vector>
 #include <cstdint>
 #include <algorithm>
@@ -34,46 +35,55 @@ namespace fwdpp
                 for (std::size_t i = 0; i < samples.size(); ++i)
                     {
                         auto s = samples[i];
-                        if (s < 0 || static_cast<std::size_t>(s) >= nnodes)
+                        if (s < 0 || s >= nnodes)
                             {
                                 throw std::out_of_range("sample list contains "
                                                         "invalid node labels");
+                            }
+                        if (sample_indexes[s] != -1)
+                            {
+                                throw std::invalid_argument(
+                                    "sample labels must be unique");
                             }
                         sample_indexes[static_cast<std::size_t>(s)]
                             = static_cast<std::int32_t>(i);
                     }
             }
 
-            inline bool
+            inline std::uint32_t
             operator()(const marginal_tree& marginal, const std::int32_t node)
             /// Returns true if node leads to any samples, false otherwise
             {
                 if (node < 0
                     || static_cast<std::size_t>(node) >= sample_indexes.size())
                     {
-                        std::cout << node << ' ' << sample_indexes.size() <<'\n';
                         throw std::out_of_range("node value out of range");
                     }
-                bool variant_in_samples = false;
+                std::uint32_t variant_in_samples = 0;
                 std::fill(std::begin(genotypes), std::end(genotypes), 0);
                 stack_top = 0;
                 node_stack[stack_top] = node;
                 while (stack_top >= 0)
                     {
                         const auto top_node = node_stack[stack_top];
+                        assert(top_node!=-1);
                         const auto sample_index = sample_indexes[top_node];
                         if (sample_index != -1)
                             {
-                                variant_in_samples = true;
+                                ++variant_in_samples;
+                                assert(genotypes[sample_index]==0);
                                 genotypes[sample_index] = 1;
                             }
                         stack_top--;
+                        std::int32_t last = -1;
                         for (auto c = marginal.left_child[top_node]; c != -1;
-                             c = marginal.right_child[c])
+                             c = marginal.right_sib[c])
                             {
                                 ++stack_top;
                                 node_stack[stack_top] = c;
+                                last = c;
                             }
+                        assert(last == marginal.right_child[top_node]);
                     }
                 return variant_in_samples;
             }
