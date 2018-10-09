83e6ac03f04d4e7d3ed5456a4156a27b05faa9e9
molpopgen <krthornt@uci.edu>
2018-10-09 15:13:00 -0700

    New header for definitions and constants

---
+++ definitions.hpp
@@ -0,0 +1,15 @@
+#ifndef FWDPP_TS_DEFINITIONS_HPP
+#define FWDPP_TS_DEFINITIONS_HPP
+
+#include <cstdint>
+
+namespace fwdpp
+{
+    namespace ts
+    {
+        using NODE_INT = std::int32_t;
+        constexpr NODE_INT NULL_NODE = -1;
+    } // namespace ts
+} // namespace fwdpp
+
+#endif
