38b957f2fe32b743b07b250b79a8f1cc48d16cf0
molpopgen <krthornt@uci.edu>
2018-10-09 15:37:54 -0700

    Fix include guard strings for consistency

--- edge.hpp
+++ edge.hpp
@@ -1,8 +1,8 @@
 //Copied from fwdpy11_arg_example.
 //Author: KRT
 //License: GPL3+
-#ifndef FWDPP_ANCESTRY_EDGE_HPP
-#define FWDPP_ANCESTRY_EDGE_HPP
+#ifndef FWDPP_TS_EDGE_HPP
+#define FWDPP_TS_EDGE_HPP
 
 #include "definitions.hpp"
 
